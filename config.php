67d49ddaf3faf2ade3ed6233f3706ff352eeeb3f
Sam Rathee <sam@rathee.ca>
2018-10-29 07:29:49 -0400

    first upload
    syntax error still persisting in index.php line 38

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
+    'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+    'client_id' => 'Q0vMvi3qOggxz5hhdnmZIYOxBegdaO0KV4rkMbM9Q9Xg5CLEm0',
+    'client_secret' => '6zY1tF9gv5jFXANCPaLLCPcULwwQ4Q3Ev09JNbpc',
+    'oauth_scope' => 'com.intuit.quickbooks.accounting',
+    'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
+)
+?>
---
+++ timeRead-apiCall.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+use QuickBooksOnline\API\DataService\DataService;
+use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
+use QuickBooksOnline\API\Facades\TimeActivity;
+
+session_start();
+
+function makeAPICall()
+{
+
+    // Create SDK instance
+    $config = include('config.php');
+    $dataService = DataService::Configure(array(
+        'auth_mode' => 'oauth2',
+        'ClientID' => $config['client_id'],
+        'ClientSecret' =>  $config['client_secret'],
+        'RedirectURI' => $config['oauth_redirect_uri'],
+        'scope' => $config['oauth_scope'],
+        'baseUrl' => "development"
+));
+
+    /*
+     * Retrieve the accessToken value from session variable
+     */
+$accessToken = $_SESSION['sessionAccessToken'];
+    /*
+     * Update the OAuth2Token of the dataService object
+     */
+$dataService->updateOAuth2Token($accessToken);
+$dataService->setLogLocation("~/Repos/Triangles");
+$dataService->throwExceptionOnError(true);
+$timeActivity = $dataService->FindbyId('timeactivity', 8);
+$error = $dataService->getLastError();
+if ($error) {
+    echo "The Status code is: " . $error->getHttpStatusCode() . "\n";
+    echo "The Helper message is: " . $error->getOAuthHelperError() . "\n";
+    echo "The Response message is: " . $error->getResponseBody() . "\n";
+}
+else {
+    echo "Created Id={$timeActivity->Id}. Reconstructed response body:\n\n";
+    $xmlBody = XmlObjectSerializer::getPostXmlFromArbitraryEntity($timeActivity, $urlResource);
+    echo $xmlBody . "\n";
+}
+
+$result = makeAPICall();
+?>
